f3fbb1205ccd80d4d7a19c515171f7d35e1a0757
Raphael Gournay <eoraptorproject@gmail.com>
2018-06-19 23:21:43 +0100

    Update to program
    I started working a bit on the program

--- statifier_code/statifier_code/Program.cs
+++ statifier_code/statifier_code/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
-namespace statifier_code
+namespace statifier
 {
+    class read
+    {
+		public string typefile = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "typesStrength.csv");
+        public string typesIndex = "typesIndex.csv";
+    }
+
+	class fetch
+    {
+
+        struct Pokémon
+        {
+            int natNum;
+            string name;
+            int type1Num;
+            int type2Num;
+            int abilityNum;
+        }
+
+        int[] attack(string typefile)
+        {
+            int[] temp;
+            StreamReader advantages = new StreamReader(typefile);
+        }
+
+        Pokémon[] GetPokémon(string fileName)
+        {
+			
+        }
+    }
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			read read = new read();
+            Console.WriteLine(read.typefile);
         }
     }
 }
+
